7181f90790a917cf4e5b6bd9c4bb4ccd0ec9d02c
loouss <loouss@qq.com>
2019-08-17 22:12:25 +0800

    composer

--- app/Http/Controller/Index.php
+++ app/Http/Controller/Index.php
@@ -4,5 +4,8 @@ namespace App\Http\Controller;
 
 class Index
 {
-
+    public function index()
+    {
+        return 'hello word!!!';
+    }
 }
--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'server_type' => 'WEB_SERVER'
+    'server_type' => 'WEB_SERVER',
+    'server_port' => 9001,
+    'server_address' => '0.0.0.0'
 ];
